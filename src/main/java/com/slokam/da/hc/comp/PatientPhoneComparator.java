7824677ad2ac2d934d692ee40cfd3d3bd59a3115
Rakesh <rakesh.chelukalapelli@gmail.com>
2024-01-10 22:02:59 +0530

    changes

--- src/main/java/com/slokam/da/hc/comp/PatientPhoneComparator.java
+++ src/main/java/com/slokam/da/hc/comp/PatientPhoneComparator.java
@@ -7,6 +7,8 @@ import com.slokam.da.hc.entity.Patient;
 public class PatientPhoneComparator implements Comparator<Patient> {
 @Override
 public int compare(Patient o1, Patient o2) {
+//	return o1.getPphone().compareTo(o2.getPphone());
+
 	return o1.getPphone().compareTo(o2.getPphone());
 }
 }
